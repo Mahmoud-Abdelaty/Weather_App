0910313b37b88fdd097daf5b3218dd2e4d416226
Mahmoud Abdelaty <126201317+Mahmoud-Abdelaty@users.noreply.github.com>
2024-02-18 21:27:04 +0200

    Add Forecast Screen

---
+++ lib/features/forecast/screens/forecast_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForeCastScreen extends StatelessWidget {
+  const ForeCastScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF080917),
+      body: Center(child: Text('ForeCastScreen')),
+    );
+  }
+}
